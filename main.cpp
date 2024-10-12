1946f7510b0d63d721abba43d41f4b57a0b0a9b8
Ilya Chvanov <ivchvanov@edu.hse.ru>
2024-10-12 18:09:08 +0300

    using smart pointers

--- main.cpp
+++ main.cpp
@@ -5,33 +5,35 @@
 #include <QVBoxLayout>
 #include <QLineEdit>
 #include <QHeaderView>
+#include <memory>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QFileSystemModel *model = new QFileSystemModel;
+
+    auto model = std::make_unique<QFileSystemModel>();
     model->setRootPath(QDir::homePath());
     model->setFilter(QDir::AllEntries | QDir::NoDotAndDotDot | QDir::Hidden);
 
-    QTreeView *treeView = new QTreeView;
-    treeView->setModel(model);
+    auto treeView = std::make_unique<QTreeView>();
+    treeView->setModel(model.get());
     treeView->setRootIndex(model->index(QDir::homePath()));
     treeView->setSortingEnabled(true);
     treeView->header()->setSectionResizeMode(QHeaderView::ResizeToContents);
 
-    QLineEdit *filterEdit = new QLineEdit;
+    auto filterEdit = std::make_unique<QLineEdit>();
     filterEdit->setPlaceholderText("Enter the name of a file or a folder");
-    QObject::connect(filterEdit, &QLineEdit::textChanged, [=](const QString &text) {
+    QObject::connect(filterEdit.get(), &QLineEdit::textChanged, [model = model.get()](const QString &text) {
         model->setNameFilters({ "*" + text + "*" });
         model->setNameFilterDisables(false);
     });
 
     QWidget window;
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(filterEdit);
-    layout->addWidget(treeView);
+    QVBoxLayout layout;
+    layout.addWidget(filterEdit.release());
+    layout.addWidget(treeView.release());
 
-    window.setLayout(layout);
+    window.setLayout(&layout);
     window.setWindowTitle(QObject::tr("File Explorer with Filtering"));
     window.setMinimumSize(800, 600);
     window.show();
